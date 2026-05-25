ae48a1b76b24f9168c97ffc729d018925629ece7
proTcity <hello@protcity.com>
2026-05-25 17:36:55 +0200

    Allow Google verification on apex domain

--- src/worker.ts
+++ src/worker.ts
@@ -8,17 +8,17 @@ export default {
   fetch(request: Request, env: Env) {
     const url = new URL(request.url);
 
-    if (url.hostname === "protcity.com") {
-      url.hostname = "www.protcity.com";
-      return Response.redirect(url.toString(), 301);
-    }
-
     if (url.pathname === "/googleca8852126a17d04f.html") {
       return new Response("google-site-verification: googleca8852126a17d04f.html", {
         headers: {
           "content-type": "text/html; charset=UTF-8"
         }
       });
+    }
+
+    if (url.hostname === "protcity.com") {
+      url.hostname = "www.protcity.com";
+      return Response.redirect(url.toString(), 301);
     }
 
     return env.ASSETS.fetch(request);
